2b72477e1c2072138165d9bd825a7c594ec9e592
Yutaro Ishiwata <yutaro.ishiwata@gmail.com>
2023-07-02 18:05:14 +0900

    refactoring main.py

--- main.py
+++ main.py
@@ -1,35 +1,47 @@
+import argparse
 import duolingo
 import inspect
-import os
-from dotenv import load_dotenv
 import glob
 import json
 
-load_dotenv()
-
-# Parse 'Nike Run Club' activity log
-total_dis = 0.0
-for file in glob.glob('activities/*.json'):
-    with open(file, 'r') as f:
-        json_data = json.loads(f.read())
-        summaries = json_data['summaries']
-        for summary in summaries:
-            if summary['metric'] == 'distance':
-                total_dis += summary['value']
+# Update data from base.md to README.md
+def update_reademe(streak_num, total_dis):
+    with open('base.md', 'r') as f:
+        base_md = f.read()
+    readme_md = base_md.replace('STREAK-NUM', str(streak_num)).replace('TOTAL-DIS', str(round(total_dis, 1)))
+    with open('README.md', 'w') as f:
+        f.write(readme_md)
 
 # Fetch 'Duolingo' streak number
-source = inspect.getsource(duolingo)
-new_source = source.replace('jwt=None', 'jwt')
-new_source = source.replace('self.jwt = None', ' ')
-exec(new_source, duolingo.__dict__)
+def get_duolingo_streak(DUOLINGO_USERNAME, DUOLINGO_JWT):
+    source = inspect.getsource(duolingo)
+    new_source = source.replace('jwt=None', 'jwt')
+    new_source = source.replace('self.jwt = None', ' ')
+    exec(new_source, duolingo.__dict__)
 
-lingo = duolingo.Duolingo(username = os.environ['DUOLINGO_USERNAME'], jwt = os.environ['DUOLINGO_JWT'])
-data = lingo.get_streak_info()
-streak_num = data.get('site_streak')
+    lingo = duolingo.Duolingo(username = DUOLINGO_USERNAME, jwt = DUOLINGO_JWT)
+    data = lingo.get_streak_info()
+    streak_num = data.get('site_streak')
+    return streak_num
 
-# Update data from base.md to README.md
-with open('base.md', 'r') as f:
-    base_md = f.read()
-readme_md = base_md.replace('STREAK-NUM', str(streak_num)).replace('TOTAL-DIS', str(round(total_dis, 1)))
-with open('README.md', 'w') as f:
-    f.write(readme_md)
+# Parse 'Nike Run Club' activity log
+def parse_nrc_activity():
+    total_dis = 0.0
+    for file in glob.glob('activities/*.json'):
+        with open(file, 'r') as f:
+            json_data = json.loads(f.read())
+            summaries = json_data['summaries']
+            for summary in summaries:
+                if summary['metric'] == 'distance':
+                    total_dis += summary['value']
+    return total_dis
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("duolingo_username", help = "usename for Duolingo")
+    parser.add_argument("duolingo_jwt", help = "API access token for Duolingo")
+    options = parser.parse_args()
+    streak_num = get_duolingo_streak(options.duolingo_username, options.duolingo_jwt)
+    total_dis = parse_nrc_activity()
+    update_reademe(streak_num, total_dis)
